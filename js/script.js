09acf28c56afaa41486a278c2527480a1ffa0c10
Dario Ghirardo <dario.ghirardo@gmail.com>
2017-12-08 00:16:59 +0100

    Minor edits

--- js/script.js
+++ js/script.js
@@ -4,7 +4,15 @@ TREEHOUSE TECHDEGREE FULL STACK JAVASCRIPT - PROJECT 3 - BUILD AN INTERACTIVE FO
 
 What the program does:
 
-It provides interactivity to a web form when registering for a conference. When the form first loads, the first textfield is highlighted. As the user makes selections in the form, parts of it are automatically updated. As the user registers for activities at the conference, the total cost is highlighted below the list. When they select an activity that conflicts with the time of another activity, the conflicting one is grayed out. They can pick different payment options and based on what they select, a different message will appear at the bottom of the page. Finally when they submit the form, if they haven't provided all the necessary information error messages will appear letting them know what they got wrong. */
+It provides interactivity to a web form when registering for a conference. When the form
+first loads, the first textfield is highlighted. As the user makes selections in the
+form, parts of it are automatically updated. As the user registers for activities at
+the conference, the total cost is highlighted below the list. When they select an activity
+that conflicts with the time of another activity, the conflicting one is grayed out.
+They can pick different payment options and based on what they select, a different message
+will appear at the bottom of the page. Finally when they submit the form, if they haven't
+provided all the necessary information error messages will appear letting them know what
+they got wrong. */
 
 
 /*==== TITLE ====*/
