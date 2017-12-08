c8d0ec4747b439ea2896d5bda61c26f76ebb4a75
Dario Ghirardo <dario.ghirardo@gmail.com>
2017-12-08 10:43:03 +0100

    Minor edits

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ What the program does:
 It provides interactivity to a web form when registering for a conference. When the form
 first loads, the first textfield is highlighted. As the user makes selections in the
 form, parts of it are automatically updated. As the user registers for activities at
-the conference, the total cost is highlighted below the list. When they select an activity
-that conflicts with the time of another activity, the conflicting one is grayed out.
+the conference, the total cost is highlighted below the list and when they select an activity
+that conflicts with the time of another one, the conflicting one is grayed out.
 They can pick different payment options and based on what they select, a different message
 will appear at the bottom of the page. Finally when they submit the form, if they haven't
 provided all the necessary information error messages will appear letting them know what
