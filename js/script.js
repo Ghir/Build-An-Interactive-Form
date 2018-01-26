f30d5d2a9bb28ef41bbc3f3e3097d4f39f114500
Dario Ghirardo <dario.ghirardo@gmail.com>
2018-01-26 13:03:05 +0100

    Update README

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-/* JS-Project-3---Build-An-Interactive-Form
-
-TREEHOUSE TECHDEGREE FULL STACK JAVASCRIPT - PROJECT 3 - BUILD AN INTERACTIVE FORM
-
-What the program does:
-
-It provides interactivity to a web form when registering for a conference. When the form
-first loads, the first textfield is highlighted. As the user makes selections in the
-form, parts of it are automatically updated. As the user registers for activities at
-the conference, the total cost is highlighted below the list and when they select an activity
-that conflicts with the time of another one, the conflicting one is grayed out.
-They can pick different payment options and based on what they select, a different message
-will appear at the bottom of the page. Finally when they submit the form, if they haven't
-provided all the necessary information error messages will appear letting them know what
-they got wrong. */
-
 
 /*==== TITLE ====*/
 const title = document.querySelector('#title');
